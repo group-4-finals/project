9d4f7bca67311f4598c4c980cd22d0b52476fd28
githubuser11999 <143807734+githubuser11999@users.noreply.github.com>
2024-11-08 07:05:42 +0000

    Pending changes exported from your codespace

---
+++ a project.cpp
@@ -0,0 +1,11 @@
+/*
+
+first is the sign up and log in page (check)
+2nd is the payment process (check)
+3rd is the timer shit (not done)
+4th is the display of games and shit (not done)
+5th is to display remaining min for example the program will display 5 min left then ask the user to topup
+6th is shutdown of the program 
+7th (when the perso wants to topup)
+
+*/
